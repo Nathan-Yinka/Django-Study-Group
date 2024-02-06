685e7ebe6fee7037272edc7e6b19ac9a3b70d4b4
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-02-06 14:14:36 +0100

    edit the default image

--- base/models.py
+++ base/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
-    avatar = models.ImageField(null=True, default="https://res.cloudinary.com/dtostqvav/image/upload/v1707223140/kgeg16zar09uqry5xdh6.svg",upload_to='discord_user_dp')
+    avatar = models.ImageField(null=True, default="avatar.svg",upload_to='discord_user_dp')
     followers = models.ManyToManyField("self", symmetrical=False, related_name="users_following", blank=True)
     following = models.ManyToManyField("self", symmetrical=False, related_name="users_followed_by", blank=True)
     
